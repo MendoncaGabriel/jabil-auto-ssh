376e2ec3c85376fb7ce1a34299d28a1cd53be0a3
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-07-04 12:36:43 -0400

    fix: correção no auto completar

--- index.mjs
+++ index.mjs
@@ -1,20 +1,19 @@
 import { Client } from 'ssh2'
-import readline from 'readline'
 import "dotenv/config"
 
 console.log(`
-==============================\n
-Bem-vindo ao JABIL SERVER SSH!\n
-==============================\n
+==============================
+Bem-vindo ao JABIL SERVER SSH!
+==============================
 `)
 
 const env = process.env
-if(
+if (
   !env["SSH_HOST"] ||
   !env["SSH_PORT"] ||
   !env["SSH_USERNAME"] ||
   !env["SSH_PASSWORD"]
-){
+) {
   throw new Error("Credenciais inválidas")
 }
 
@@ -23,16 +22,16 @@ const conexao = new Client()
 conexao.on('ready', () => {
   console.log('✅ Conexão SSH estabelecida. Iniciando terminal interativo...\n')
 
-  conexao.shell((err, stream) => {
+  conexao.shell({ term: 'xterm-color' }, (err, stream) => {
     if (err) throw err
 
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-      prompt: '> ',
-    })
+    // Deixa o terminal em modo raw para funcionar tab, ctrl + c, etc
+    process.stdin.setRawMode(true)
+    process.stdin.resume()
+    process.stdin.pipe(stream)
+    stream.pipe(process.stdout)
 
-    // Envia os comandos automáticos ao entrar
+    // Comandos automáticos ao entrar
     stream.write('cd ..\n')
     stream.write('cd ..\n')
     stream.write('cd opt/\n')
@@ -40,38 +39,18 @@ conexao.on('ready', () => {
     stream.write(env["ROOT_PASSWORD"] + '\n')
     setTimeout(() => {
       stream.write('clear\n')
-    }, 500);
-
-    // Mostra a saída vinda do servidor
-    stream.on('data', (data) => {
-      process.stdout.write(data.toString())
-    })
-
-    stream.stderr.on('data', (data) => {
-      process.stderr.write(`❌ STDERR: ${data.toString()}`)
-    })
+    }, 500)
 
     stream.on('close', () => {
       console.log('\n🔒 Conexão encerrada.')
       conexao.end()
-      rl.close()
+      process.stdin.setRawMode(false)
+      process.stdin.pause()
     })
-
-    // Captura comandos digitados no terminal local
-    rl.on('line', (line) => {
-      stream.write(line.trim() + '\n')
-    })
-
-    rl.on('SIGINT', () => {
-      console.log('\n❌ Encerrando conexão...')
-      stream.end('exit\n')
-    })
-
-    rl.prompt()
   })
 }).connect({
   host: env["SSH_HOST"],
-  port: env["SSH_PORT"],
+  port: Number(env["SSH_PORT"]),
   username: env["SSH_USERNAME"],
   password: env["SSH_PASSWORD"],
 })
